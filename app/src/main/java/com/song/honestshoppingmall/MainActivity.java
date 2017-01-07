b1b1cc8836a500eef3695975c550db60d8e834b7
ys <111111>
2017-01-07 11:16:48 +0800

    杨宋提交代码

--- app/src/main/java/com/song/honestshoppingmall/MainActivity.java
+++ app/src/main/java/com/song/honestshoppingmall/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("张业松提交代码");
         System.out.println("熊向东提交测试代码");
         System.out.println("刘宗瓒提交测试代码");
+        System.out.println("杨宋鹏程提交测试代码");
 
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
