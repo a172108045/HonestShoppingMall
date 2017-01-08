c83f13518c3b92d69ce09301f6b43f4908d9d744
Judy19920322 <522483446@qq.com>
2017-01-08 09:35:01 +0800

    把主界面的五个Fragment写出来了，切换逻辑也搞定了，现在每个人写各自的fragment和业务就行了

--- app/src/main/java/com/song/honestshoppingmall/activity/HomeActivity.java
+++ app/src/main/java/com/song/honestshoppingmall/activity/HomeActivity.java
@@ -3,29 +3,76 @@ package com.song.honestshoppingmall.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import com.song.honestshoppingmall.event.FirstEvent;
 import com.song.honestshoppingmall.R;
+import com.song.honestshoppingmall.fragment.CategoryFragment;
+import com.song.honestshoppingmall.fragment.HomeFragment;
+import com.song.honestshoppingmall.fragment.MineFragment;
+import com.song.honestshoppingmall.fragment.SettingFragment;
+import com.song.honestshoppingmall.fragment.ShopCartFragment;
 
-import org.greenrobot.eventbus.EventBus;
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private LinearLayout mActivitySecond;
+    private FrameLayout mFlHome;
+    private ImageView mIvHome;
+    private ImageView mIvCategory;
+    private ImageView mIvShopcart;
+    private ImageView mIvMine;
+    private ImageView mIvSetting;
 
-public class HomeActivity extends AppCompatActivity {
 
-    private Button btn_FirstEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        btn_FirstEvent = (Button) findViewById(R.id.btn_first_event);
+        initView();
+        initData();
 
-        btn_FirstEvent.setOnClickListener(new View.OnClickListener() {
+    }
 
-            @Override
-            public void onClick(View v) {
-                EventBus.getDefault().post(new FirstEvent("HomeActivity"));
-            }
-        });
+    private void initView() {
+        mActivitySecond = (LinearLayout) findViewById(R.id.activity_second);
+        mFlHome = (FrameLayout) findViewById(R.id.fl_home);
+        mIvHome = (ImageView) findViewById(R.id.iv_home);
+        mIvCategory = (ImageView) findViewById(R.id.iv_category);
+        mIvShopcart = (ImageView) findViewById(R.id.iv_shopcart);
+        mIvMine = (ImageView) findViewById(R.id.iv_mine);
+        mIvSetting = (ImageView) findViewById(R.id.iv_setting);
+    }
+
+    private void initData() {
+        mIvHome.setOnClickListener(this);
+        mIvCategory.setOnClickListener(this);
+        mIvShopcart.setOnClickListener(this);
+        mIvMine.setOnClickListener(this);
+        mIvSetting.setOnClickListener(this);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fl_home, new HomeFragment()).commit();
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl_home, new HomeFragment()).commit();
+                break;
+            case R.id.iv_category:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl_home, new CategoryFragment()).commit();
+                break;
+            case R.id.iv_shopcart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl_home, new ShopCartFragment()).commit();
+                break;
+            case R.id.iv_mine:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl_home, new MineFragment()).commit();
+                break;
+            case R.id.iv_setting:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fl_home, new SettingFragment()).commit();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/song/honestshoppingmall/fragment/SettingFragment.java
@@ -0,0 +1,23 @@
+package com.song.honestshoppingmall.fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by Judy on 2017/1/8.
+ */
+
+public class SettingFragment extends BaseFragment {
+    @Override
+    protected View initView(LayoutInflater inflater) {
+        TextView tv = new TextView(mContext);
+        tv.setText("这是更多");
+        return tv;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
