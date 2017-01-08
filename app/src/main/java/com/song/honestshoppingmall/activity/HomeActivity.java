f70fbddf38b4bf4de1be428c57b1b75c7d4abc8d
Judy19920322 <522483446@qq.com>
2017-01-08 11:14:54 +0800

    首页的侧滑界面以及标题完成，杨宋写首页其余部分

--- app/src/main/java/com/song/honestshoppingmall/activity/HomeActivity.java
+++ app/src/main/java/com/song/honestshoppingmall/activity/HomeActivity.java
@@ -75,4 +75,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
+
 }
--- app/src/main/java/com/song/honestshoppingmall/fragment/HomeFragment.java
+++ app/src/main/java/com/song/honestshoppingmall/fragment/HomeFragment.java
@@ -1,23 +1,66 @@
 package com.song.honestshoppingmall.fragment;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.song.honestshoppingmall.R;
+import com.song.honestshoppingmall.activity.HomeActivity;
 
 /**
  * Created by Judy on 2017/1/8.
  */
 
-public class HomeFragment extends BaseFragment {
+public class HomeFragment extends BaseFragment implements View.OnClickListener {
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private ImageView mIv_menu_home;
+    private ImageView mIv_cart_home;
+
     @Override
     protected View initView(LayoutInflater inflater) {
-        TextView tv = new TextView(mContext);
-        tv.setText("这是首页");
-        return tv;
+        View view = View.inflate(mContext, R.layout.fragment_home, null);
+        mDrawerLayout = (DrawerLayout) view.findViewById(R.id.drawer_main);
+        mNavigationView = (NavigationView) view.findViewById(R.id.nav_view);
+        mIv_menu_home = (ImageView) view.findViewById(R.id.iv_menu_home);
+        mIv_cart_home = (ImageView) view.findViewById(R.id.iv_cart_home);
+
+
+
+        return view;
     }
 
     @Override
     protected void initData() {
+        mIv_menu_home.setOnClickListener(this);
+        mIv_cart_home.setOnClickListener(this);
 
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mDrawerLayout.closeDrawers();
+                return false;
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_menu_home:
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+                break;
+
+            case R.id.iv_cart_home:
+                ((HomeActivity)mContext).getSupportFragmentManager().beginTransaction().replace(R.id.fl_home, new ShopCartFragment()).commit();
+                break;
+        }
     }
 }
