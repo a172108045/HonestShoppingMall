99596e7e6d33ea55782df8b830a33f17190aa38e
zan <zanzan303088965@qq.com>
2017-01-12 10:52:01 +0800

    修改addAddressfragmaent名称改为AddAdddressFragment

---
+++ app/src/main/java/com/song/honestshoppingmall/fragment/AddAddressFragment.java
@@ -0,0 +1,22 @@
+package com.song.honestshoppingmall.fragment;
+
+import android.view.View;
+
+import com.song.honestshoppingmall.R;
+
+/**
+ * Created by zan on 2017/1/12.
+ */
+
+public class AddAddressFragment extends BaseFragment {
+    @Override
+    protected View initView() {
+        View view = View.inflate(mContext, R.layout.fragment_address_add, null);
+        return view;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
