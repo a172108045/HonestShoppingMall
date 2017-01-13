2cc8a40c0b5b0624801c983764ca85e54bb83814
zan <zanzan303088965@qq.com>
2017-01-13 09:00:34 +0800

    代码更新!

--- app/src/main/java/com/song/honestshoppingmall/fragment/AddAddressFragment.java
+++ app/src/main/java/com/song/honestshoppingmall/fragment/AddAddressFragment.java
@@ -1,6 +1,8 @@
 package com.song.honestshoppingmall.fragment;
 
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
 
 import com.song.honestshoppingmall.R;
 import com.song.honestshoppingmall.activity.HomeActivity;
@@ -10,15 +12,35 @@ import com.song.honestshoppingmall.activity.HomeActivity;
  */
 
 public class AddAddressFragment extends BaseFragment {
+
+    private PopupWindow mPopupWindow;
+
     @Override
     protected View initView() {
-        ((HomeActivity)mContext).changeTitle("添加地址");
+        ((HomeActivity) mContext).changeTitle("添加地址");
         View view = View.inflate(mContext, R.layout.fragment_address_add, null);
+
+        initOnClick();
         return view;
+    }
+
+    private void initOnClick() {
+
     }
 
     @Override
     protected void initData() {
 
+        initPopupWindow();
     }
+
+    private void initPopupWindow() {
+        View contentView = View.inflate(mContext, R.layout.popupwindow_addaddress, null);
+
+        int width = ViewGroup.LayoutParams.WRAP_CONTENT;
+        int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        mPopupWindow = new PopupWindow(contentView, width, height, true);
+    }
+
+
 }
